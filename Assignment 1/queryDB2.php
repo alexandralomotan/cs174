a2859308c6e50fd8e7d322150cd2ac7233ffbda8
amle92 <andyle1992@yahoo.com>
2015-09-07 14:46:55 -0700

    Update queryDB2.php

--- Assignment 1/queryDB2.php
+++ Assignment 1/queryDB2.php
@@ -9,36 +9,32 @@ $password = 'pwcs174';
                            PDO::ERRMODE_EXCEPTION);
 		
 		$restaurant = filter_input(INPUT_POST, "restaurantName");
-        //print "$restaurant has been added";
 		
 		$query = "SELECT *
-			      FROM restaurants";
+			      FROM restaurants
+				  ORDER BY `Restaurant Name`";
+				  
 					  
-	
 		print "<table border='1'>\n";
+		
+		
+		//if a user enters a restaurant name, then query data from it.
+		if((strlen($restaurant)) > 0){
+		$query = "SELECT *
+		          FROM `restaurants`
+				  WHERE `Restaurant Name` = ('$restaurant')";
+	    }
+		
 		
 		$result = $con->query($query);
 	    $result ->setFetchMode(PDO::FETCH_ASSOC);
 		$row = $result->fetch();
 		
-		if((strlen($restaurant)) > 0){
-		//$query = "INSERT INTO `restaurants`(`Restaurant Name`)
-		//          VALUES ('$restaurant')";
-		$query = "SELECT *
-		          FROM `restaurants`
-				  WHERE `Restaurant Name` = ('$restaurant')";
-				  
-				  //$data = $con->query($query);
-	              //$data->setFetchMode(PDO::FETCH_ASSOC);
-	    }
-		
-		
-		//$query = "SELECT *
-		//	      FROM restaurants";
-		
 		$data = $con->query($query);
 	    $data->setFetchMode(PDO::FETCH_ASSOC);
 				  
+				  
+		//prints out the header for the table		  
 		$header = true;
 		foreach($data as $row) {
 			if($header){
@@ -51,7 +47,7 @@ $password = 'pwcs174';
 			
 			$header = false;
 			}
-			
+			//prints out the data for the table
 			print "              <tr>\n";
 			foreach($row as $name => $value) {
 			    print "                <td>$value</td>\n";	
