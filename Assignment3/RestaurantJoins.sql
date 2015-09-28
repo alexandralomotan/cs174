78757d6be18dea2e868c823bfcb780f59b80fa1f
Anthony Tsang <atsang.matterport.com>
2015-09-28 13:20:09 -0700

    Added RestaurantJoins.sql

---
+++ Assignment3/RestaurantJoins.sql
@@ -0,0 +1,24 @@
+#describe City;
+#describe Cuisine;
+#describe Price_Range;
+#describe Restaurant;
+
+# Select All Restaurants
+SELECT Restaurant.name as 'Restaurant', 
+       Cuisine.name as 'Cuisine', 
+	   City.name as 'City', 
+       Price_Range.name as 'Price Range'
+FROM Restaurant, Cuisine, City, Price_Range
+WHERE Restaurant.cuisine_fk = Cuisine.id
+      AND Restaurant.city_fk = City.id
+      AND Restaurant.price_range_fk = Price_Range.id;
+
+# Select Restaurants only located in Fremont      
+SELECT Restaurant.name as 'Restaurant', 
+	   City.name as 'City', 
+       Price_Range.name as 'Price Range'
+FROM Restaurant, Cuisine, City, Price_Range
+WHERE Restaurant.cuisine_fk = Cuisine.id
+      AND Restaurant.city_fk = City.id
+      AND Restaurant.price_range_fk = Price_Range.id
+      AND City.name = "Fremont";
