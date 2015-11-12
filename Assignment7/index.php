447ca43c84741956e8b0870b5097f09d5b78a6c3
Alexandra Lomotan <alexandralomotan@gmail.com>
2015-11-11 17:53:01 -0800

    added ajax

---
+++ Assignment7/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+	<link rel="stylesheet" href="./css/layout.css" type="text/css" />
+	<link rel="stylesheet" href="./css/styles.css" type="text/css" />
+  <link rel="stylesheet" href="jquery-ui-1.11.4.custom/jquery-ui.theme.min.css" type="text/css" />
+  <link rel="stylesheet" href="./jquery-ui-1.11.4.custom/jquery-ui.css" type="text/css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <title>Index</title>
+</head>
+
+<body>
+    <?php
+        include "header.php";
+	      include "navbar.php";
+	      include "middleindex.php";
+	      include "footer.php";
+    ?>
+</body>
+
+</html>
---
+++ Assignment7/navbar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+	<link rel="stylesheet" href="./css/styles.css" type="text/css" />
+  <script type="text/javascript" src="js/hover.js"></script>
+</head>
+
+<body>
+    <div id="nav">
+	<li><a href="./index.php">Home</a></li>
+	<li><a href="./restaurants.php">Restaurants</a></li>
+	<li><a href="./simple_web.php">Simple_Web</a></li>
+    </div>
+</body>
+
+</html>
